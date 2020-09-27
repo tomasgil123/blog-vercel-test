af4d39b362a5057c6f3b314c1f934fda853e9fd0
Tomas Gil <tomasgil1234@gmail.com>
2020-09-27 18:52:36 -0300

    refactor: styles post

--- src/domain/post/postBody.tsx
+++ src/domain/post/postBody.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown/with-html'
 import styled from 'styled-components'
+import { space, breakpoints } from 'src/tokens'
 
 import Code from './code'
 
@@ -8,11 +9,17 @@ const PostContainer = styled.div`
   width: 100%;
   margin: auto;
   max-width: 600px;
+  padding-left: ${space.s4};
+  padding-right: ${space.s4};
   img {
     max-width: 600px;
   }
   p {
     line-height: 1.8;
+    font-size: ${space.s4};
+    @media (min-width: ${breakpoints.md}) {
+      font-size: ${space.s5};
+    }
   }
 `
 
